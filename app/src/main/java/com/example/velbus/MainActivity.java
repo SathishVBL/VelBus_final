9358706d11f4a96caccf51052fbf26b81b72f713
vsath <vsathishbl2002@gmail.com>
2024-03-07 09:57:16 +0530

    this is my first project

--- app/src/main/java/com/example/velbus/MainActivity.java
+++ app/src/main/java/com/example/velbus/MainActivity.java
@@ -1,14 +1,19 @@
 // MainActivity.java
 package com.example.velbus;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,24 +21,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // MainActivity.java
+        MotionLayout motionLayout = findViewById(R.id.mainlayout);
 
-        Button searchBtn = findViewById(R.id.startBTN);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                findViewById(R.id.textView1).startAnimation(AnimationUtils.loadAnimation(MainActivity.this,R.anim.text2));
+                findViewById(R.id.textView2).startAnimation(AnimationUtils.loadAnimation(MainActivity.this,R.anim.text1));
 
-        searchBtn.setOnClickListener(v -> {
-            // When searchBtn is clicked, start SecondActivity
-            Intent intent = new Intent(MainActivity.this, searchpage.class);
-            startActivity(intent);
+//                findViewById(R.id.textView1).startAnimation(AnimationUtils.loadAnimation(MainActivity.this,R.anim.text1));
+//                findViewById(R.id.textView2).startAnimation(AnimationUtils.loadAnimation(MainActivity.this,R.anim.text2));
 
-            // Optionally, you can finish() the current activity if you don't want to come back to it
-            // finish();
-        });
+                motionLayout.transitionToEnd();
+
+
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        Intent intent = new Intent(MainActivity.this, searchpage.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }, 4000); // 4 seconds delay to run the intent after the exectuin of motionLayout.transitionToEnd();
+            }
+        }, 400); // 400 initial delay to run the motionLayout.transitionToEnd(); and inner hanlder.
+
+
+
+
 
 
     }
-    public void onclickSeeDirection(View view){
-        Intent intent=new Intent(MainActivity.this, see_direction_mapview.class);
-        startActivity(intent);
-    }
+//    public void onclickSeeDirection(View view){
+//        Intent intent=new Intent(MainActivity.this, see_direction_mapview.class);
+//        startActivity(intent);
+//        finish();
+//    }
 
     // The rest of your MainActivity code...
 
